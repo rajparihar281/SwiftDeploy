fc7572bd58fe423f7dd9e926af4c41f337606868
rajparihar281 <mrrajparihar281@gmail.com>
2026-01-26 15:55:01 +0530

    Fix test to match webhook response

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,7 +4,7 @@ def test_home():
     client = app.app.test_client()
     response = client.get("/")
     assert response.status_code == 200
-    assert b"CI/CD MVP App is running!" in response.data
+    assert b"WEBHOOK CI/CD WORKING" in response.data
 
 def test_health():
     client = app.app.test_client()
